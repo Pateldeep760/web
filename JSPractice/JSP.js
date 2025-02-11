52d0a337aa6c881d129b784a3c1a5dd922c75ac5
Pateldeep760 <pateldeep760@gmail.com>
2025-02-11 23:40:45 +0530

    First Commit

---
+++ JSPractice/JSP.js
@@ -0,0 +1,49 @@
+
+
+function show1(){
+    document.getElementById('overlay0').style.display= 'block'
+    document.getElementById('OverlayIMG').src='./Assets/Image  (1).jpg'
+}
+function show2(){
+    document.getElementById('overlay0').style.display= 'block'
+    document.getElementById('OverlayIMG').src='./Assets/Image  (2).jpg'
+}
+function show3(){
+    document.getElementById('overlay0').style.display= 'block'
+    document.getElementById('OverlayIMG').src='./Assets/Image  (3).jpg'
+}
+function show4(){
+    document.getElementById('overlay0').style.display= 'block'
+    document.getElementById('OverlayIMG').src='./Assets/Image  (4).jpg'
+}
+function show5(){
+    document.getElementById('overlay0').style.display= 'block'
+    document.getElementById('OverlayIMG').src='./Assets/Image  (5).jpg'
+}
+function show6(){
+    document.getElementById('overlay0').style.display= 'block'
+    document.getElementById('OverlayIMG').src='./Assets/Image  (6).jpg'
+}
+function show7(){
+    document.getElementById('overlay0').style.display= 'block'
+    document.getElementById('OverlayIMG').src='./Assets/Image  (7).jpg'
+}
+function show8(){
+    document.getElementById('overlay0').style.display= 'block'
+    document.getElementById('OverlayIMG').src='./Assets/Image  (8).jpg'
+}
+function show9(){
+    document.getElementById('overlay0').style.display= 'block'
+    document.getElementById('OverlayIMG').src='./Assets/Image  (9).jpg'
+}
+
+
+
+function ab(){
+    document.getElementById('overlay0').style.display='none';
+}
+
+
+
+
+
